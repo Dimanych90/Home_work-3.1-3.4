5defe4e6f99de2c1e17cedd5ea342e041eb62fee
Dmitry Medelyaev <dimanych90@gmail.com>
2020-03-08 16:48:43 +0300

    Поправил немного но все равно оштбки

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-require 'functions.php';
 
 //задача 1
 $file = file_get_contents('data.xml');
@@ -28,33 +27,51 @@ echo '<br>';
 echo '<hr>';
 //адача 2
 
-$name = ['name' => 'Dima', 'age' => 29, 'gender' => 'man'];
-echo json_encode($name) . '<br>';
-file_put_contents('output.json', $name);
+$original = ['user' => ['name' => 'Vasya'], 'system' => ['of' => 'down']];
+$original = json_decode('output.json', true);
+file_put_contents('output.json', $original);
 echo file_get_contents('output.json') . '<br>';
-$namestr = '{"name":"Dima","age":29,"gender":"man"}';
+$originalstr = '{"user":{"name":"Vasya"},"system":{"of":"down"}}';
 echo '<pre>';
-print_r(json_decode($namestr, false));
-file_put_contents('output2.json', $namestr);
+print_r(json_decode($originalstr, false));
+file_put_contents('output2.json', $originalstr);
 echo file_get_contents('output2.json') . '<br>';
+if (rand(0, 1)) {
+    unset($original['system']);
+}
+$copy = json_decode('output2.json', true);
+$difference = array_diff_assoc($original, $copy);
+if (!empty($difference)) {
+    print_r($difference);
+} else {
+    echo 'Массивы не отличаются';
+}
+
 
 echo '<br>';
 echo '<hr>';
 // задача 3
 
-foreach (range(0, 100, 2) as $num) {
-    $arr = str_split($num, 3);
-    echo '<pre>';
-    print_r($arr);
-    $newfile = fopen('justfile.csv', 'w');
-
-    fputcsv($newfile, $arr, ';');
-
+for ($i = 0; $i <= 50; $i++) {
+    $numbers[$i] = rand(1, 100);
 }
+file_put_contents('numbers.csv', '');
+$csv = fopen('numbers.csv', 'r+');
+fputcsv($csv, 'numbers.csv', $numbers, ';');
+fclose('numbers.csv');
+$csv = fopen('numbers.csv', 'r+');
+$numbers = fgetcsv($csv, 1000, ';');
+fclose('numbers.csv');
+$sum = array_reduce($numbers, function ($result, $number) {
+    if ($number % 2 === 0) {
+        return $result += $number;
+    }
+}, 0);
+echo "Сумма четных чисел: $sum";
 
 // задача 4
 
 
 $file4 = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json'); // открываем файл
-$array1=json_decode($file4,true);
+$array1 = json_decode($file4, true);
 echo var_dump($file4);
