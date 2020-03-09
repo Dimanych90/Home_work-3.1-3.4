b6e8e750638aad00df4054b7a3b935b7db09bb04
Dmitry Medelyaev <dimanych90@gmail.com>
2020-03-09 18:24:23 +0300

    Тот вариант где все правильно

--- index.php
+++ index.php
@@ -28,19 +28,17 @@ echo '<hr>';
 //адача 2
 
 $original = ['user' => ['name' => 'Vasya'], 'system' => ['of' => 'down']];
-$original = json_decode('output.json', true);
-file_put_contents('output.json', $original);
-echo file_get_contents('output.json') . '<br>';
-$originalstr = '{"user":{"name":"Vasya"},"system":{"of":"down"}}';
-echo '<pre>';
-print_r(json_decode($originalstr, false));
-file_put_contents('output2.json', $originalstr);
-echo file_get_contents('output2.json') . '<br>';
+//кодируем в json, потом пишем в файл
+file_put_contents('output.json', json_encode($original));
+$original = json_decode(file_get_contents('output.json'), true);
+//кодируем копию в json, потом пишем в файл
+file_put_contents('output2.json', json_encode($original));
 if (rand(0, 1)) {
     unset($original['system']);
 }
-$copy = json_decode('output2.json', true);
-$difference = array_diff_assoc($original, $copy);
+//достаем копию
+$copy = json_decode(file_get_contents('output2.json'), true);
+$difference = array_diff_assoc($copy, $original); //сравниваем копию с оригиналом, а не оригинал с копией, здесь я сам немного удивился.
 if (!empty($difference)) {
     print_r($difference);
 } else {
@@ -57,21 +55,27 @@ for ($i = 0; $i <= 50; $i++) {
 }
 file_put_contents('numbers.csv', '');
 $csv = fopen('numbers.csv', 'r+');
-fputcsv($csv, 'numbers.csv', $numbers, ';');
-fclose('numbers.csv');
+fputcsv($csv, $numbers, ';');
+fclose($csv);
 $csv = fopen('numbers.csv', 'r+');
 $numbers = fgetcsv($csv, 1000, ';');
-fclose('numbers.csv');
+fputcsv($csv, $numbers, ';');
+fclose($csv);
 $sum = array_reduce($numbers, function ($result, $number) {
     if ($number % 2 === 0) {
         return $result += $number;
     }
 }, 0);
 echo "Сумма четных чисел: $sum";
+echo '<br>';
+echo '<hr>';
 
 // задача 4
 
 
-$file4 = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json'); // открываем файл
+$file4 = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&titles=
+Main%20Page&prop=revisions&rvprop=content&format=json'); // открываем файл
 $array1 = json_decode($file4, true);
-echo var_dump($file4);
+
+echo $array1['query']['pages']['15580374']['title']. '<br>';
+echo $array1['query']['pages']['15580374']['pageid'];
